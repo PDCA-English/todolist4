2938d948c91e73698b8b5951dd1dad81aa30a68f
Takuya <r28.6sa@gmail.com>
2021-05-01 13:24:55 +0900

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>Todo List</h1>
+    <form>
+      <input type="text" style="display:none" />
+      <input type="text" />
+      <input type="button" value="追加" />
+    </form>
+    <table align="center" border="0">
+      <tr>
+        <td>
+          <input type="text" />
+        </td>
+        <td>
+          <input type="button" value="更新" />
+        </td>
+        <td>
+          <input type="button" value="削除" />
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+</script>
